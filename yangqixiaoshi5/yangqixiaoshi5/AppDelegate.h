675cf69d580c2f41eade3d43e1b113f6663de45a
jyz <jyz@jyzdeiMac.local>
2017-11-12 17:47:45 +0800

    yangqixiaoshi5

---
+++ yangqixiaoshi5/yangqixiaoshi5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yangqixiaoshi5
+//
+//  Created by jyz on 2017/11/12.
+//  Copyright © 2017年 jyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yangqixiaoshi5/yangqixiaoshi5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yangqixiaoshi5
+//
+//  Created by jyz on 2017/11/12.
+//  Copyright © 2017年 jyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
